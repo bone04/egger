fd3b934dfec76f41c1bcb8e583f8f24d4e3a305d
Bone4Coder <88817100+bone04@users.noreply.github.com>
2024-09-25 14:38:02 +0800

    home page 1
    create homepage

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,7 @@
 export default function HomePage() {
   return (
-
-    <div>Hello World ! </div>
-    
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
-
-  
 }
